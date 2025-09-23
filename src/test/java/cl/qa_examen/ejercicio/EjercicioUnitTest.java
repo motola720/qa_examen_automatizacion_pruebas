6a7c523ad0d093ed0748e95024aaf84acdff1610
motola720 <motola.th@gmail.com>
2025-09-23 20:50:38 -0300

    chore: bootstrap (Maven + JUnit + .gitignore)

---
+++ src/test/java/cl/qa_examen/ejercicio/EjercicioUnitTest.java
@@ -0,0 +1,10 @@
+package cl.qa_examen.ejercicio;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class EjercicioUnitTest {
+  @Test void suma_basica() {
+    assertEquals(5, 2 + 3);
+  }
+}
